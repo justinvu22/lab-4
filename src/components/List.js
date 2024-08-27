74fe7773a81843802c394e29c80e5476f7d43177
justinvu22 <112003801+justinvu22@users.noreply.github.com>
2024-08-27 17:05:04 +1000

    pass the array into this file

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,17 @@
+function Task(props){
+  return(
+    <li>{props.description} <input type= "checkbox" checked = "false"/></li>
+  );
+}
+
 function List(props) {
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				<Task description = {props.tasks[0].description}/>
+				<Task description = {props.tasks[1].description}/>
+				<Task description = {props.tasks[2].description}/>
 			</ul>
 		</div>
 	);
