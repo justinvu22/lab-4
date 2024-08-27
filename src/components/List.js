f4e14c67a9df8ef7809319ebcfcdd252264167ce
justinvu22 <112003801+justinvu22@users.noreply.github.com>
2024-08-27 17:37:03 +1000

    feat: add a toggle for the list, removing hard code,and remove the loop

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,38 @@
-function Task(props){
-  return(
-    <li>{props.description} <input type= "checkbox" checked = "false"/></li>
+function Task(props) {
+  return (
+    <li>
+      <input 
+        type="checkbox" 
+        checked={props.completed}
+        onChange={() => props.onToggle(props.id)}
+      />
+      {props.description}
+    </li>
   );
 }
 
 function List(props) {
-	return (
-		<div>
-			<h1>{ props.heading }</h1>
-			<ul>
-				<Task description = {props.tasks[0].description}/>
-				<Task description = {props.tasks[1].description}/>
-				<Task description = {props.tasks[2].description}/>
-			</ul>
-		</div>
-	);
+  const handleToggle = (taskId) => {
+
+    props.onTaskToggle(taskId);
+  };
+
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <ul>
+        {props.tasks.map((task) =>
+          <Task
+            key={task.id}
+            id={task.id}
+            description={task.description}
+            completed={task.completed}
+            onToggle={handleToggle}
+          />
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default List;
