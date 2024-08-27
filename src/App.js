0edb1eadecc265d6d22bc61b2f8702c3cf506c29
justinvu22 <112003801+justinvu22@users.noreply.github.com>
2024-08-27 17:37:51 +1000

    implement the toggle and import the REACT library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React, { useState } from 'react';
 import List from './components/List';
 import './App.css';
 
-const tasks = [
-  { description: "Learn React", completed: true },
-  {description: "Learn JSX", completed: false},
-  {description: "Build a React App", completed: false}
-];
-
 function App() {
-    return (
-        <div className='page'>     
-            <List heading='My Tasks' tasks= {tasks}/>
-        </div>
-    );
+  const [tasks, setTasks] = useState([
+    { id: 1, description: "Learn React", completed: false },
+    { id: 2, description: "Build an app", completed: false },
+    { id: 3, description: "Learn JSX", completed: false },
+  ]);
+
+  const onTaskToggle = (taskId) => {
+    setTasks(tasks.map(task =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    ));
+  };
+
+  return (
+    <List
+      heading="My Tasks"
+      tasks={tasks}
+      onTaskToggle={onTaskToggle}
+    />
+  );
 }
 
 export default App;
+
+
